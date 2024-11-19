dc8808cc7679d64719704e1111f6c753b0603ebb
Zaur <zaurik87@gmail.com>
2024-11-19 17:26:28 +0400

    removal myBatis error

--- tasklist_security_expression/src/main/java/com/example/tasklist/web/controller/TaskController.java
+++ tasklist_security_expression/src/main/java/com/example/tasklist/web/controller/TaskController.java
@@ -8,6 +8,7 @@ import com.example.tasklist.web.validation.OnUpdate;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +25,7 @@ public class TaskController {
 
     @GetMapping("/{id}")
     @Operation(summary = "Get task by id")
+    @PreAuthorize("canAccessTask(#id)")
     public TaskDto getById(@PathVariable Long id) {
         Task task = taskService.getById(id);
         return taskMapper.toDto(task);
@@ -31,6 +33,7 @@ public class TaskController {
 
     @PutMapping
     @Operation(summary = "Update task")
+    @PreAuthorize("canAccessTask(#taskDto.id)")
     public TaskDto update(@Validated(OnUpdate.class)
                           @RequestBody TaskDto taskDto) {
         Task task = taskMapper.toEntity(taskDto);
@@ -40,6 +43,7 @@ public class TaskController {
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete task by id")
+    @PreAuthorize("canAccessTask(#id)")
     public void deleteById(@PathVariable Long id) {
         taskService.delete(id);
     }
